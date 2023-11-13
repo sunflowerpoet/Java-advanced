bf1e39d5544c8277cfa813fa6851770949b499f6
sunflowerpoet <anna.simionato747@gmail.com>
2023-11-13 13:16:52 +0100

    first commit advanced

--- src/Es_01/Main.java
+++ src/Es_01/Main.java
@@ -1,41 +1,23 @@
 package Es_01;
-import java.util.Scanner;
-
 /*
 - Scrivere una funzione che controlli se un numero int è in un determinato range
 - Se lo è ritorna true se non lo è lancia un'eccezione
 */
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         try {
-            int number = insertNumber(scanner);
-            System.out.println("Input number: " + number);
-            System.out.println("Number is in range.");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            scanner.close();
+            System.out.print("Input number: ");
+            checkRange(10);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
-
-    private static int insertNumber(Scanner scanner) {
-        int number;
-        do {
-            System.out.print("Insert input number: ");
-            while (!scanner.hasNextInt()) {
-                System.out.println("Insert valid number.");
-                scanner.next();
-            }
-            number = scanner.nextInt();
-
-            if (number < 0 || number > 10) {
-                throw new IllegalArgumentException("Error: number must be within range (0 - 10).");
-            }
-        } while (number < 0 || number > 10);
-        return number;
+    public static void checkRange(int number) throws Exception {
+        if (number < 0 || number > 10) {
+            throw new Exception("Error: number not in range.");
+        } else {
+            System.out.println(number);
+            System.out.println("Number within range (0 - 10).");
+        }
     }
 }
--- src/Es_02/Main.java
+++ src/Es_02/Main.java
@@ -1,28 +1,22 @@
 package Es_02;
-import java.util.Scanner;
 /*
 Scrivere una funzione che controlli se un carattere è un numero altrimenti lanciare una eccezione.
 */
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        char inputChar = 'f';
         try {
-            char character = insertChar(scanner);
-            System.out.println("Input char number: " + character);
-            System.out.print("Correct input.");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            scanner.close();
+            checkChar(inputChar);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
-    private static char insertChar(Scanner scanner) {
-        System.out.print("Please, insert numeric character: ");
-        String input = scanner.nextLine();
-
-        if (input.length() != 1 || !Character.isDigit(input.charAt(0))) {
-            throw new IllegalArgumentException("Error, character invalid. Please insert numeric character.");
+    public static void checkChar (char inputChar) throws Exception {
+        if (Character.isDigit(inputChar)) {
+            System.out.println("Character is a digit.");
+        } else {
+            throw new Exception();
         }
-        return input.charAt(0);
+
     }
 }
